9166258ceb31808e55eae24ce87cee903e88baf0
Khan <twer.wet@mail.ru>
2023-03-09 00:47:49 +0700

    release v1

---
+++ todoapp/todolist/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField('Название задания', max_length=500)  #задания
+    is_complete = models.BooleanField("Завершено", default=False)
+    user = models.CharField("name", max_length=50)
+
+    class Meta:
+        verbose_name = "Задание"
+        verbose_name_plural = "Задания"
+
+    def __str__(self):
+        return  self.title

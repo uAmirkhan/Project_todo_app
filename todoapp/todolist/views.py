c6ea2ce2996c0168ac7c11e3c2809d97b30f4d84
uAmirkhan <amrumaev@mail.ru>
2023-03-15 23:34:05 +0700

    function add and update task

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,7 +1,25 @@
-from django.http import HttpResponse
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from .models import ToDo
+
 
 def index(request):
-    return HttpResponse("Hello World!")
+    todos = ToDo.objects.all()
+    return render(request, 'todoapp/index.html', {'todo_list': todos, 'title': 'Главная страница'})
+
+
+@require_http_methods(['POST'])
+def add(request):
+    title = request.POST['title']  #
+    todo = ToDo(title=title)
+    todo.save()                    # сохранение бд
+    return  redirect('index')      # возвращаемся на главную страницу
+
+def update(request, todo_id):
+    todo = ToDo.objects.get(id=todo_id)  #ищем по айди таску
+    todo.is_complete = not todo.is_complete # поле "выполнено" в false
+    todo.save()
+    return redirect('index')

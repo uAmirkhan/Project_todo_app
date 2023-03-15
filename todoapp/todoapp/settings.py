1505d41bf7f8924ececb3808c1c1a335d972fc61
uAmirkhan <amrumaev@mail.ru>
2023-03-16 01:17:25 +0700

    release

--- todoapp/todoapp/settings.py
+++ todoapp/todoapp/settings.py
@@ -117,9 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+# базовый путь к каталогу статики проекта
 STATIC_URL = 'static/'
-STATIC_URL = os.path.join(BASE_DIR, STATIC_URL)
 
+# Абсолютный путь к каталогу статики, в котором с помощью python manage.py collectstatic
+# будут собираться статические файлы.
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+# C:\\Users\\mike\\pythonProject\\To-Do-Django\\todoapp\\static/
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
